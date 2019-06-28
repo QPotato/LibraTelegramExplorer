c803661f5a04915910cda774fe9c0e29fcb981a4
QPotato <federicobadaloni@hotmail.com>
2019-06-28 10:48:06 -0300

    details

--- src/explorer.ts
+++ src/explorer.ts
@@ -1,4 +1,4 @@
-import { LibraClient, LibraNetwork, AccountState } from 'libra-core'; import * as TelegramBot from 'node-telegram-bot-api';
+import { LibraClient, LibraNetwork, AccountState, LibraWallet } from 'libra-core'; import * as TelegramBot from 'node-telegram-bot-api';
 
 if (process.env.TELEGRAM === undefined)
     throw new Error('No telegram token in environment');
@@ -6,29 +6,32 @@ const telegramtoken = process.env.TELEGRAM;
 const bot = new TelegramBot(telegramtoken);
 
 const libraClient = new LibraClient({ network: LibraNetwork.Testnet });
-const AddressLength = 64
+const AddressLength = 64;
 
 interface Reaction {
     pattern: (msg: TelegramBot.Message) => boolean;
     action: (msg: TelegramBot.Message) => void;
 }
 
+
 // The bot will try to match the patterns sequentially and execute only the first one that matches.
 export const reactions: Reaction[] = [
     {
         pattern: (msg) => msg.text !== undefined && msg.text.trim().length === AddressLength,
         action: (msg) => {
-            if (msg.text === undefined)
-                throw new Error
             const accountAddress = msg.text.trim();
             libraClient.getAccountState(accountAddress)
                 .then((accountState : AccountState) => {
                     // const reply = `*Balance: ${accountState.balance}`
-                    const reply = `*Account Details:*\nBalance: ${accountState.balance} Libra\nSequence nº: ${accountState.sequenceNumber}\nSent Event Count: ${accountState.sentEventsCount}\nReceived Event Count ${accountState.receivedEventsCount}`
+                    const reply = `*Account Details:*\nBalance: ${accountState.balance.dividedToIntegerBy(1000000).decimalPlaces(4)} Libra\nSequence nº: ${accountState.sequenceNumber}\nSent Event Count: ${accountState.sentEventsCount}\nReceived Event Count ${accountState.receivedEventsCount}`
                     bot.sendMessage(msg.chat.id, reply, {parse_mode: 'Markdown'});
                 })
-                .catch(() => bot.sendMessage(msg.chat.id, 'Error contacting the Libra network.s'));
+                .catch(() => bot.sendMessage(msg.chat.id, 'Error contacting the Libra network'));
         }
+    },
+    {
+        pattern: (msg) => msg.text !== undefined && parseInt(msg.text.trim()) !== NaN,
+        action: (msg) => bot.sendMessage(msg.chat.id, 'Transactions soon.')
     },
     {
         pattern: () => true,
